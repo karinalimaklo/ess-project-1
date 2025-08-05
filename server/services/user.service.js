56687c4232056231efe9915757ef51489f99a5eb
Luiz Gustavo <lgscss@cin.ufpe.br>
2025-08-05 19:08:30 -0300

    feat(user): add getUserById route and update service method for user retrieval

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -6,8 +6,10 @@ class UserService {
     return await User.find({ status: { $ne: 'Excluído' } });
   }
   static async getUserById(userId) {
-    const user = await User.findById(userId);
+    const user = await User.findOne({ _id: userId, status: { $ne: 'Excluído' } });
+
     if (!user) {
+
       throw new Error('Usuário não encontrado.');
     }
     return user;
