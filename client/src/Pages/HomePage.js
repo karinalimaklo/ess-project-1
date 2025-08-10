25c729dcfec6244f4649eca4da539ab1edcf6bac
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-08-09 22:42:52 -0300

    feat(homepage): tela home sem reviews cadastradas

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,28 +3,9 @@ import SearchBar from "../components/SearchBar/searchBar";
 import SearchResults from "../components/SearchResults/SearchResults";
 
 export default function HomePage() {
-  const [results, setResults] = useState([]);
-  const [hasSearched, setHasSearched] = useState(false);
-  const [searchType, setSearchType] = useState('');
-
-  // função que recebe os dados e o tipo de busca do SearchBar
-  const handleSearchResults = (data, type) => {
-    setResults(data);
-    setSearchType(type);
-    setHasSearched(true); // Marca que uma busca foi realizada
-  };
-
-    return (
-        <div className="App-body">
-
-            <SearchBar onResults={handleSearchResults} />
-      
-            <SearchResults
-                results={results}
-                searchType={searchType}
-                hasSearched={hasSearched}
-            />
-
-        </div>
-    );
+  return (
+    <div className="App-body" style={{ background: "white", color: "black" }}>
+      <h1>Ainda não foi cadastrada nenhuma review</h1>
+    </div>
+  );
 }
