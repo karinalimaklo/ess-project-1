b4f045407c40a081798108e3e4bd469b3165126b
Karina Lima <155447732+karinalimaklo@users.noreply.github.com>
2025-08-10 14:20:50 -0300

    fix: EditarPage.jsx

--- client/src/music/EditarPage.jsx
+++ client/src/music/EditarPage.jsx
@@ -6,6 +6,7 @@ import CoverUploader from "../components/CadastroModal/CoverUploader";
 import SuccessMessage from "../components/CadastroModal/SuccessMessage";
 import FailMessage from "../components/CadastroModal/FailMessage";
 import Button from "../components/Button/Button";
+import Header from "../components/Header/Header";
 
 function EditarMusicaPage() {
   const { id } = useParams();
