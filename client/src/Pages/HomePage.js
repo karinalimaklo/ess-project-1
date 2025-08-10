b3ae534e2c42dd958eb1d3f7923d96d4ce24ba9e
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-08-10 19:21:40 -0300

    feat(homepage): novo componente para card de review

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -3,9 +3,20 @@ import SearchBar from "../components/SearchBar/searchBar";
 import SearchResults from "../components/SearchResults/SearchResults";
 
 export default function HomePage() {
+  const reviews = [
+    { user: "Tulio", musica: "Let Down" },
+    { user: "Lira", musica: "Famous" },
+  ];
   return (
     <div className="App-body" style={{ background: "white", color: "black" }}>
-      <h1>Ainda não foi cadastrada nenhuma review</h1>
+      {reviews?.length === 0 ? (
+        <h1>Ainda não foi cadastrada nenhuma review</h1>
+      ) : (
+        <div>
+          {/* {reviews?.map((review) => {return })} */}
+
+        </div>
+      )}
     </div>
   );
 }
---
+++ client/src/components/ReviewCard/ReviewCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardWrapper from '../Card/Card';
+import Rating from '@mui/material/Rating';
+
+export default function ReviewCard({ review }) {
+  return (
+    <Card>
+        <Typography component="legend">{review.musica}</Typography>
+        <Rating name="read-only" value={review.nota} readOnly/>
+        <br/>
+        <br/>
+        <Typography component="legend">{review.user}</Typography>
+    </Card>
+  );
+}
