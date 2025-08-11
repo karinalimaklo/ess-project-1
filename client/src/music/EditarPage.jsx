09976b85bf3d4974dec03c27c5eddf557c5e078b
Karina Lima <155447732+karinalimaklo@users.noreply.github.com>
2025-08-11 09:32:29 -0300

    fix: EditarPage.jsx

--- client/src/music/EditarPage.jsx
+++ client/src/music/EditarPage.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "./CadastroPage.css";
@@ -6,7 +7,7 @@ import CoverUploader from "../components/CadastroModal/CoverUploader";
 import SuccessMessage from "../components/CadastroModal/SuccessMessage";
 import FailMessage from "../components/CadastroModal/FailMessage";
 import Button from "../components/Button/Button";
-import Header from "../components/Header/Header";
+import Header from '../components/Header/Header';
 
 function EditarMusicaPage() {
   const { id } = useParams();
@@ -117,6 +118,10 @@ function EditarMusicaPage() {
   };
 
   return (
+    <>
+        <Header 
+        onMenuClick={() => console.log("Abrir menu lateral")}
+        />
     <div className="cadastro-container">
       <h1>EDITAR MÚSICA</h1>
       <form onSubmit={handleSubmit} className="cadastro-form">
@@ -243,7 +248,7 @@ function EditarMusicaPage() {
           </div>
 
           <div className="finalizar-cadastro">
-            <Button type="submit">Salvar alterações</Button>
+            <Button type="submit">SALVAR ALTERAÇÕES</Button>
           </div>
         </div>
 
@@ -265,7 +270,7 @@ function EditarMusicaPage() {
       </form>
       {showSuccessMessage && (
         <SuccessMessage
-          message={"MÚSICA ATUALIZADA\nCOM SUCESSO!"}
+          message={"MÚSICA ATUALIZADA COM SUCESSO!"}
           buttonText="VOLTAR AOS DETALHES DA MÚSICA"
           onClose={() => navigate(`/details/${id}`)}
         />
@@ -278,6 +283,7 @@ function EditarMusicaPage() {
         />
       )}
     </div>
+    </>
   );
 }
 
