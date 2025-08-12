eb994af6f777533ac79510be462db751096afd65
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-08-12 13:33:43 -0300

    test(homepage): added cancel operation to homepage tests

--- cypress/e2e/features/homepage.test.js
+++ cypress/e2e/features/homepage.test.js
@@ -9,12 +9,15 @@ import { setAdmin, setCommonUser } from "../../../client/src/currentUser";
 // 	I want to remover uma review da home
 // 	So that ela não fique mais disponível para os usuários
 
-// Scenario: Administrador remove review com sucesso
-//   Given eu estou na página inicial logado como "administrador"
-//   And eu vejo uma review "Let Down" de "Túlio"
-//   When eu clico para excluir review de "Let Down" feita por “Túlio”
-//   Then a review "Let Down" de "Túlio" é removida da página
 
+
+
+// Scenario: Administrador cancela remoção de review
+// 	 Given eu estou na página inicial logado como "administrador"
+// 	 And eu vejo uma review "Baby" de "Paulo Miranda"
+// 	 When eu clico para excluir review de "Baby" feita por "Paulo Miranda"
+//      And cancelo a operação
+// 	 Then a review "Baby" de "Paulo Miranda" continua na página
 Given("eu estou na página inicial logado como {string}", (role) => {
   if (role === "administrador") {
     setAdmin();
@@ -30,20 +33,55 @@ Given("eu vejo uma review {string} de {string}", (songTitle, authorName) => {
   cy.get('[data-cy="review-card"]').contains(authorName).should("be.visible");
 });
 
-When("eu clico para excluir review de {string} feita por {string}", (songTitle, authorName) => {
+When(
+  "eu clico para excluir review de {string} feita por {string}",
+  (songTitle, authorName) => {
     cy.get('[data-cy="review-card"]')
-      .contains(songTitle).parent().parent().contains(authorName).within(() => {
+      .contains(songTitle)
+      .parent()
+      .parent()
+      .contains(authorName)
+      .parent()
+      .within(() => {
         cy.get('[data-cy="delete-button"]').click();
-      })
-    // Confirma a exclusão no modal
-    // cy.get('[data-cy="discard-modal]"').should("be.visible");
-    cy.get('[data-cy="confirm-delete-button"]').click();
+      });
   }
 );
+
+
+
+When("cancelo a operação", () => {
+  cy.get('[data-cy="cancel-delete-button"]').click();
+});
+
+Then(
+  "a review {string} de {string} continua na página",
+  (songTitle, authorName) => {
+    cy.get('[data-cy="review-card"]').contains(songTitle).parent().parent().contains(authorName).should("exist");
+  }
+);
+
+// Scenario: Administrador remove review com sucesso
+//   Given eu estou na página inicial logado como "administrador"
+//   And eu vejo uma review "Let Down" de "Túlio"
+//   When eu clico para excluir review de "Let Down" feita por “Túlio”
+//   Then a review "Let Down" de "Túlio" é removida da página
+
+When("confirmo a operação", () => {
+  // Confirma a exclusão no modal
+  // cy.get('[data-cy="discard-modal]"').should("be.visible");
+  cy.get('[data-cy="confirm-delete-button"]').click();
+});
 
 Then(
   "a review {string} de {string} é removida da página",
   (songTitle, authorName) => {
-    cy.get('[data-cy="review-card"]').contains(songTitle).parent().parent().contains(authorName).should("not.exist");
+    cy.get('[data-cy="review-card"]')
+      .contains(songTitle)
+      .parent()
+      .parent()
+      .contains(authorName)
+      .should("not.exist");
   }
 );
+
