9b373800ba6eccbaa3ebeb96ee2a69fd71e2a514
Túlio Carvalho Lira <tulio.lira@outlook.com>
2025-08-12 09:09:49 -0300

    refactor(homepage): added rowsize const to make changes easier

--- client/src/components/GroupReviewCards/GroupReviewCards.jsx
+++ client/src/components/GroupReviewCards/GroupReviewCards.jsx
@@ -5,6 +5,8 @@ import { getAllReviews } from "../../services/reviews";
 import Button from "@mui/material/Button"; // import do botão MUI
 import currentUser from "../../currentUser"; // import do usuário atual
 
+const rowSize = 5; // Número de cards por linha
+
 export default function GroupReviewCards() {
   const [reviews, setReviews] = useState([]);
   const [showAll, setShowAll] = useState(false);
@@ -21,7 +23,7 @@ export default function GroupReviewCards() {
   }, []);
 
   const displayedReviews =
-    !showAll && reviews.length > 3 ? reviews.slice(0, 3) : reviews;
+    !showAll && reviews.length > rowSize ? reviews.slice(0, rowSize) : reviews;
 
   return (
     <div
@@ -32,25 +34,25 @@ export default function GroupReviewCards() {
         <h1>Ainda não foi cadastrada nenhuma review</h1>
       ) : (
         <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-          {Array.from({ length: Math.ceil(displayedReviews.length / 3) }).map(
-            (_, rowIdx) => (
-              <div
-                key={rowIdx}
-                style={{ display: "flex", flexDirection: "row", gap: 10 }}
-              >
-                {displayedReviews
-                  .slice(rowIdx * 3, rowIdx * 3 + 3)
-                  .map((review, idx) => (
-                    <ReviewCard
-                      key={rowIdx * 3 + idx}
-                      review={review}
-                      isAdmin={currentUser?.isAdmin} // Verifica se o usuário atual é admin
-                    />
-                  ))}
-              </div>
-            )
-          )}
-          {!showAll && reviews.length > 3 && (
+          {Array.from({
+            length: Math.ceil(displayedReviews.length / rowSize),
+          }).map((_, rowIdx) => (
+            <div
+              key={rowIdx}
+              style={{ display: "flex", flexDirection: "row", gap: 10 }}
+            >
+              {displayedReviews
+                .slice(rowIdx * rowSize, rowIdx * rowSize + rowSize)
+                .map((review, idx) => (
+                  <ReviewCard
+                    key={rowIdx * rowSize + idx}
+                    review={review}
+                    isAdmin={currentUser?.isAdmin}
+                  />
+                ))}
+            </div>
+          ))}
+          {!showAll && reviews.length > rowSize && (
             <Button
               variant="outlined"
               onClick={() => setShowAll(true)}
